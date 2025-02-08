79f92df2a3582428152fa36d5dae2e38067a5a1e
Oliver Knox <hi@oliverknox.io>
2025-02-08 17:54:53 +0000

    Omit forked repos and improve name formatting

--- utils/github/get-repos.ts
+++ utils/github/get-repos.ts
@@ -10,13 +10,15 @@ export async function getRepos(): Promise<GitHubRepo[]> {
 	}
 
 	const response = await octokit.repos.listForUser({ username: "oliverrknox" });
-	const repos = response.data.map((repo) => {
-		return {
-			name: repo.name.includes(".") ? repo.name : titleCase(repo.name), // Preserve case for website repo names but enforce Title Case for regular names
-			description: repo.description,
-			url: repo.html_url
-		};
-	});
+	const repos = response.data
+		.filter((repo) => !repo.fork)
+		.map((repo) => {
+			return {
+				name: repo.name.includes(".") ? repo.name : titleCase(repo.name), // Preserve case for website repo names but enforce Title Case for regular names
+				description: repo.description,
+				url: repo.html_url
+			};
+		});
 	cache.set("github-repos", repos);
 	return repos;
 }
--- utils/strings/title-case.ts
+++ utils/strings/title-case.ts
@@ -1,5 +1,6 @@
 export function titleCase(str: string) {
 	return str
+		.replace("-", " ")
 		.split(" ")
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
 		.join(" ");
