ee5cd7e9984e12a930b0443fb336783dbfe2dbd0
Oliver Knox <hi@oliverknox.io>
2025-02-08 12:05:21 +0000

    Move `/media` inside `/public`

--- collections/media.ts
+++ collections/media.ts
@@ -3,7 +3,7 @@ import { CollectionConfig } from "payload";
 export const Media: CollectionConfig = {
 	slug: "media",
 	upload: {
-		staticDir: "media",
+		staticDir: "public/media",
 		mimeTypes: ["image/*"]
 	},
 	fields: [
